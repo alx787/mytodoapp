222eef260186e0cb04ce1fb74b43df826460322a
alx787 <kaanova@gmail.com>
2021-02-04 15:16:44 +0300

    add res draw

--- app/src/main/java/ru/alx/todoapp/FullscreenActivity.kt
+++ app/src/main/java/ru/alx/todoapp/FullscreenActivity.kt
@@ -19,6 +19,8 @@ class FullscreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        setContentView(R.layout.activity_fullscreen)
+
       }
 
 
